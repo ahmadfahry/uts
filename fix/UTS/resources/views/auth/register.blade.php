3806ef8d78ee0605b5d23eb9fa7ad7702cb91993
ahmadfahry <ahmadfahrysk8@gmail.com>
2021-04-13 23:04:24 +0700

    Custom login username & password

--- fix/UTS/resources/views/auth/register.blade.php
+++ fix/UTS/resources/views/auth/register.blade.php
@@ -12,6 +12,21 @@
                         @csrf
 
                         <div class="form-group row">
+                            <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
+
+                                @error('username')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <!-- Name -->
+                        <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
@@ -25,6 +40,7 @@
                             </div>
                         </div>
 
+                        <!-- Email -->
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
@@ -38,7 +54,8 @@
                                 @enderror
                             </div>
                         </div>
-
+                        
+                        <!-- Password -->
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
@@ -53,6 +70,7 @@
                             </div>
                         </div>
 
+                        <!-- Confirm Password -->
                         <div class="form-group row">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
@@ -61,6 +79,7 @@
                             </div>
                         </div>
 
+                        <!-- Tombol Register -->
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
